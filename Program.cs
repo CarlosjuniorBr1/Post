a6f1c3e789ac48ee875a5ddda6764f2e30aafb0e
CarlosjuniorBr1 <carlos.alberto369@outlook.com>
2022-09-29 08:05:43 -0300

    Simulando post em uma blog

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("WOw that's awesome!");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Travelling to New Zealand",
+                "I'm going to visit this wondeeful country!",
+                5
+                );
+           p1.AddComment(c1);
+           p1.AddComment(c2);
+
+
+            Comment c3 = new Comment("Good Night!");
+            Comment c4 = new Comment("May the Force be with you");
+
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good Night Guys",
+                "See you tomorrou",
+                12
+                ); 
+
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+
+
+
+
+        }
+    }
+}
